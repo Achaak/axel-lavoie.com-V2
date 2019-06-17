da31323f1607388e63e1d5e24ef6db7f4623d0f0
Achak <axel.lavoie@gmail.com>
2019-06-17 23:22:48 +0200

    dev

--- www/src/views/development/js/circle_bar.js
+++ www/src/views/development/js/circle_bar.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
-    var test = circleBar().init(
+    
+    test = circleBar();
+    
+    test.init(
         "#skills", 
         { 
             text: "test",
-            lineColor: "red",
-            lineBgColor: "white",
-            lineWidth: "10px",
-            lineWidthHover: "20px",
-            diameter: "200px",
-            backgroundColor: "pink"
+            lineColor: "#1d8eff",
+            lineBgColor: "rgb(150, 203, 255)",
+            backgroundColor: "white",
+            lineWidth: "15",
+            lineWidthHover: "7px",
+            bgLineWidth: "10",
+            bgLineWidthHover: "4",
+            lineDuration: "2s",
+            lineDelay: "1s",
+            diameter: "300",
+            viewPourcentage: true,
+            textCSS: {
+                color: "red"
+            },
+            textHoverCSS: {
+                color: "pink"
+            },
+            pourcentageCSS: {
+                color: "red"
+            },
+            pourcentageHoverCSS: {
+                color: "pink"
+            }
         },
-        85);
+        50);
 });
